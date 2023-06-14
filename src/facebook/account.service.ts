6e83a5634b5d81496b8d508b38df2637af50f30d
HM <hieumdd@gmail.com>
2023-06-14 20:59:03 +0700

    feat: Initial commit

---
+++ src/facebook/account.service.ts
@@ -0,0 +1,23 @@
+import { getClient } from './api.service';
+
+type ListAccountsResponse = { data: { account_id: string; id: string }[] };
+
+export const getAccounts = async () => {
+    const client = await getClient();
+
+    const BUSINESS_ID = 819752709175788;
+
+    return Promise.all(
+        ['client_ad_accounts', 'owned_ad_accounts'].map(async (edge) => {
+            return client
+                .request<ListAccountsResponse>({
+                    method: 'GET',
+                    params: { limit: 500 },
+                    url: `/${BUSINESS_ID}/${edge}`,
+                })
+                .then((response) => response.data.data);
+        }),
+    ).then((accountGroups) => {
+        return accountGroups.flatMap((accounts) => accounts.map(({ account_id }) => account_id));
+    });
+};
---
+++ src/facebook/facebook.service.test.ts
@@ -0,0 +1,30 @@
+import { CAMPAIGN_INSIGHTS } from './pipeline.const';
+import { createPipelineTasks, runPipeline } from './pipeline.service';
+
+it('pipeline', async () => {
+    return runPipeline(
+        {
+            accountId: '798134188164544',
+            start: '2023-06-01',
+            end: '2023-07-01',
+        },
+        CAMPAIGN_INSIGHTS,
+    )
+        .then((results) => expect(results).toBeDefined())
+        .catch((error) => {
+            console.error({ error });
+            return Promise.reject(error);
+        });
+});
+
+it('create-tasks', async () => {
+    return createPipelineTasks({
+        start: '2023-05-01',
+        end: '2023-06-01',
+    })
+        .then((result) => expect(result).toBeDefined())
+        .catch((error) => {
+            console.error({ error });
+            return Promise.reject(error);
+        });
+});
